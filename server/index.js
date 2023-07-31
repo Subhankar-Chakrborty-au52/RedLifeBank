91542c035ca4244b319f79d52a19eb2619ba9284
Your Name <subhankarchakrborty@gmail.com>
2023-07-31 11:34:16 +0530

    first route commited

--- server/index.js
+++ server/index.js
@@ -1,1 +1,30 @@
-console.log("hello world")
+// Import all
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const dbConnect = require("./config/database");
+
+//db connect
+dbConnect();
+
+
+const app = express();
+
+
+//middlewares
+app.use(express.json());
+
+
+
+//routes
+app.use("/api/v1/auth", require("./routes/authRoutes"))
+
+
+//Load config file from env
+const PORT = process.env.PORT || 5001;
+
+
+// server connection
+app.listen(PORT, () => {
+  console.log("server is running successfully");
+});
---
+++ server/models/userModel.js
@@ -0,0 +1,69 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+  role: {
+    type: String,
+    required: [true, "role is required"],
+    enum: ["admin", "organization", "user", "hospital"],
+  },
+  name: {
+    type: String,
+    required: function () {
+      if (this.role === "user" || this.role === "admin") {
+        return true;
+      }
+      return false;
+    },
+  },
+  organizationName: {
+    type: String,
+    required: function () {
+      if (this.role === "organization") {
+        return true;
+      }
+      return false;
+    },
+  },
+  organizationName: {
+    type: String,
+    required: function () {
+      if (this.role === "organization") {
+        return true;
+      }
+      return false;
+    },
+  },
+ 
+  hospitalName:{
+    type: String,
+    required: function(){
+        if(this.role === 'hospital'){
+            return true
+        }
+        return false
+    }
+  },
+
+  email: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  website: {
+    type: String,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+},{timestamps:true});
+
+module.exports = mongoose.model('users', userSchema);
